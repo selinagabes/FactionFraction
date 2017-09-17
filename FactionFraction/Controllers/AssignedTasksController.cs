cc5a4aacb70df478917b960ce748c041126aa537
Joel Rorseth <joelrorseth@gmail.com>
2017-09-16 22:31:18 -0400

    Implement untested task matching logic

--- FactionFraction/Controllers/AssignedTasksController.cs
+++ FactionFraction/Controllers/AssignedTasksController.cs
@@ -176,5 +176,75 @@ namespace FactionFraction.Controllers
         private double DetermineWorkPercentage(int[] grades, int grade) {
             return grade / SumGrades(grades);
         } 
+
+
+
+        // Calculate estimated completion time for each task 
+        private void CalculateEstimatedTimeForTasks(List<AssignedTask> tasks, List<GroupMember> members) {
+
+            List<int> taskTotal = new List<int>();
+            List<int> timeTotal = new List<int>();
+
+            for (int i = 0; i < tasks.Count; ++i) {
+                foreach (var member in members) {
+                    taskTotal.Add(member.ProposedMinutes.ToList()[i].Length);
+                }
+
+                // Set estimated completion time for this task
+                // TODO: Save to entity
+                tasks[i].EstimatedMinutes = (int) Math.Round(taskTotal.Average());
+                taskTotal.Clear();
+            }
+        }
+
+
+        // Determine the completion time for the whole project based already defined estimated times
+        private double EstimatedProjectCompletionTime(List<AssignedTask> tasks) {
+            double totalTime = 0;
+
+            // Sum the average times for all tasks (should already be set!!)
+            for (int i = 0; i < tasks.Count; ++i) {
+                totalTime += tasks[i].EstimatedMinutes;
+            }
+
+            return totalTime;
+        }
+
+
+        // Map tasks to group members based exclusively on task completion estimations
+        private void AssignTasks(List<AssignedTask> tasks, List<GroupMember> members) {
+
+
+            // Sort the tasks ascending in estimated completion time
+            tasks = tasks.OrderBy(task => task.EstimatedMinutes).ToList();
+
+            List<GroupMember> candidates = members;
+
+            // Find lowest completion time amongst all candidates for each task
+            for (var i = 0; i < tasks.Count; ++i) {
+                GroupMember bestMember = new GroupMember();
+
+                // If all members have been assigned, "refill" candiates for more tasks!
+                if (candidates.Count == 0) { 
+                    candidates = members; 
+                }
+
+                foreach (var member in candidates) {
+
+                    // If this is the shortest proposed completion time, update new shortest
+                    if (member.ProposedMinutes.ToList()[i].Length < bestMember.ProposedMinutes.ToList()[i].Length) {
+                        bestMember = member;
+                    }
+                }
+                
+
+                // Set this task's group member, add task to group member's task list
+                // TODO: Save entity
+                tasks[i].GroupMember = bestMember;
+                bestMember.AssignedTasks.Add(tasks[i]);
+
+                candidates.Remove(bestMember);
+            }
+        }
     }
 }
